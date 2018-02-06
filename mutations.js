05a80ea9c48f620042554e68c4c437725e4ac3a0
danielnet <danielnett@hotmail.com>
2018-02-06 08:34:03 +0100

    Code challenge - Mutations - Check string on another string -

---
+++ mutations.js
@@ -0,0 +1,17 @@
+//Mutations
+//Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+function mutation(arr) {
+  
+  var e1 = arr[0].toLowerCase();
+  var e2 = arr[1].toLowerCase();
+  
+  for (i=0;i<e2.length;i++) {
+    if (e1.indexOf(e2[i]) == -1)
+      return false;
+  }
+  
+  return true;
+ }
+
+
+mutation(["voodoo", "no"]);
